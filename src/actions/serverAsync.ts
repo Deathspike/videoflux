f33d084eff8356e087d75c6301e319b591ef853b
Deathspike <meister.deathspike@outlook.com>
2023-11-07 18:57:49 +0100

    Add webui to server

--- src/actions/serverAsync.ts
+++ src/actions/serverAsync.ts
@@ -1,13 +1,16 @@
 import * as app from '..';
 import * as fastify from 'fastify';
+import * as path from 'node:path';
 import {Data} from './schemas/Data';
 import {FromSchema} from 'json-schema-to-ts';
 import {Queue} from './classes/Queue';
 const packageData = require('../../package');
 
 export async function serverAsync(options: app.Options) {
+  const root = path.join(__dirname, '../../public');
   const queue = await Queue.createAsync('queue.json', createHandler(options));
   const server = fastify.default();
+  server.register(require('@fastify/static'), {root});
   server.route(get(queue));
   server.route(post(queue));
   await server.listen({host: '0.0.0.0', port: 8670});
